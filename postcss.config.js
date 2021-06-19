aabda6acc5f24ecedb22568926d3a7b4ae39f4f9
xiaodeng <927561237@qq.com>
2021-06-20 00:14:21 +0800

    引用组件库Vant和REM适配

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/* 
+   postcss 的配置文件
+*/
+
+module.exports = {
+  plugins: {
+    'postcss-pxtorem': {
+      rootValue: 37.5,
+      propList: ['*'],
+    },
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <h3>头条</h3>
     <!-- 路由的出口 就是被匹配到的路由，那个页面就会渲染到这里 -->
     <router-view />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+    <div class="hhh">hhh</div>
   </div>
 </template>
 
@@ -10,4 +20,10 @@
 export default {}
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.hhh {
+  width: 200px;
+  height: 200px;
+  background: skyblue;
+}
+</style>
