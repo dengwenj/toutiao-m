5403710de1ab5f3817523be7cbe24a567154202c
xiaodeng <927561237@qq.com>
2021-06-19 22:43:14 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h3>头条</h3>
+    <!-- 路由的出口 就是被匹配到的路由，那个页面就会渲染到这里 -->
+    <router-view />
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {}
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    // 给路径取别名
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
