138aafcbb1a2f696294597d362ee19bdb66d3c59
xiaodeng <927561237@qq.com>
2021-06-22 17:03:16 +0800

    用户退出-清除用户登录状态

--- src/views/my/compschild/BottomCell.vue
+++ src/views/my/compschild/BottomCell.vue
@@ -2,7 +2,12 @@
   <div class="">
     <van-cell class="cell" title="消息通知" is-link url="/" />
     <van-cell class="cell" title="小邓同学" is-link to="/qa" />
-    <van-cell v-if="userToken" class="drop-out" title="退出登录" />
+    <van-cell
+      v-if="userToken"
+      class="drop-out"
+      title="退出登录"
+      @click="onLogOut"
+    />
   </div>
 </template>
 
@@ -25,7 +30,22 @@ export default {
   watch: {},
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    onLogOut() {
+      this.$dialog
+        .confirm({
+          title: '退出提示',
+          message: '是否确定退出？',
+        })
+        .then(() => {
+          // 确定退出  清除用户登录状态
+          this.$store.commit('setUser', null)
+        })
+        .catch(() => {
+          // 取消退出
+        })
+    },
+  },
 }
 </script>
 
