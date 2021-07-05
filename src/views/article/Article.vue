768e015afe4130bc99a684e3d6a8ce8f7eb763f5
xiaodeng <927561237@qq.com>
2021-07-05 20:46:21 +0800

    页面布局-标题和作者信息

--- src/views/article/Article.vue
+++ src/views/article/Article.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="article-container">文章详情</div>
+  <div class="article-container">
+    <!-- 文章详情导航 -->
+    <van-nav-bar
+      class="app-nav-bar"
+      title="文章详情"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /文章详情导航 -->
+
+    <!-- 文章详情标题 -->
+    <h1 class="title">牛逼程序员都用的开源工具，你用了 几个？</h1>
+    <!-- /文章详情标题 -->
+
+    <!-- 文章详情用户信息 -->
+    <user-info-article />
+    <!-- /文章详情用户信息 -->
+  </div>
 </template>
 
 <script>
+import UserInfoArticle from './compschild/UserInfoArticle'
+
 export default {
   name: 'Article',
-  components: {},
+  components: {
+    UserInfoArticle,
+  },
 
   // 在组件中获取动态路由参数
   //     方式一：this.$route.params.articleId
@@ -28,4 +49,13 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.article-container {
+  .title {
+    font-size: 20px;
+    padding: 15px;
+    color: #333;
+    background-color: #fff;
+  }
+}
+</style>
---
+++ src/views/article/compschild/UserInfoArticle.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="user-info">
+    <van-cell center class="user-cell">
+      <van-image
+        class="user-img"
+        slot="icon"
+        fit="cover"
+        round
+        src="https://img01.yzcdn.cn/vant/cat.jpeg"
+      />
+      <div slot="title" class="user-name">天涯小型客</div>
+      <div slot="label" class="user-time">14小时前</div>
+      <van-button class="attention" round type="info" size="small" icon="plus"
+        >关注</van-button
+      >
+    </van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped lang="less">
+.user-cell {
+  .user-img {
+    width: 35px;
+    height: 35px;
+    margin-right: 8px;
+  }
+  .user-name {
+    font-size: 12px;
+    color: #333;
+  }
+  .user-tiem {
+    font-size: 12px;
+    color: #b4b4b4;
+  }
+  .van-cell__label {
+    margin-top: 0;
+  }
+  .attention {
+    width: 85px;
+    height: 30px;
+  }
+}
+</style>
