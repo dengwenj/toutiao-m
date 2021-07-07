69237427ef5d7d17c6f4b996dc134aa59cc367be
xiaodeng <927561237@qq.com>
2021-07-07 20:47:21 +0800

    编辑资料-页面布局

--- src/views/user-profile/UserProfile.vue
+++ src/views/user-profile/UserProfile.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="user-profile">编辑资料</div>
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-nav-bar"
+      title="注册 / 登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+
+    <van-cell title="头像" is-link center>
+      <van-image
+        width="35"
+        height="35"
+        fit="cover"
+        round
+        src="https://img01.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" is-link value="hello"></van-cell>
+    <van-cell title="性别" is-link value="男"></van-cell>
+    <van-cell title="生日" is-link value="2021-5-1"></van-cell>
+  </div>
 </template>
 
 <script>
