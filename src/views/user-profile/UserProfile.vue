60a405a0966f244fec12dc454590963b36b35802
xiaodeng <927561237@qq.com>
2021-07-07 21:52:20 +0800

    修改昵称-页面布局

--- src/views/user-profile/UserProfile.vue
+++ src/views/user-profile/UserProfile.vue
@@ -12,9 +12,13 @@
     <van-cell title="头像" is-link center>
       <van-image width="35" height="35" fit="cover" round :src="user.photo" />
     </van-cell>
-    <van-cell title="昵称" is-link :value="user.name" />
+    <van-cell title="昵称" is-link :value="user.name" @click="name = !name" />
     <van-cell title="性别" is-link :value="user.gender === 0 ? '男' : '女'" />
     <van-cell title="生日" is-link :value="user.birthday" />
+
+    <!-- 修改昵称 -->
+    <update-name :name="name" @cross="name = !name" />
+    <!-- /修改昵称 -->
   </div>
 </template>
 
@@ -22,13 +26,18 @@
 // 网络请求
 import { userProfile } from 'api/user'
 
+import UpdateName from './compschild/UpdateName'
+
 export default {
   name: '',
-  components: {},
+  components: {
+    UpdateName,
+  },
   props: {},
   data() {
     return {
       user: {}, // 用户资料
+      name: false, // 是否展示用户昵称
     }
   },
   computed: {},
---
+++ src/views/user-profile/compschild/UpdateName.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="update-name">
+    <van-popup v-model="name" position="bottom" :style="{ height: '100%' }">
+      <van-nav-bar
+        title="修改昵称"
+        left-text="取消"
+        right-text="完成"
+        @click-left="$emit('cross')"
+      />
+      <div class="field">
+        <van-field
+          v-model="message"
+          rows="2"
+          autosize
+          type="textarea"
+          maxlength="50"
+          placeholder="请输入留言"
+          show-word-limit
+        />
+      </div>
+    </van-popup>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {
+    name: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped lang="less">
+.van-popup {
+  background-color: #f5f7f9;
+  .field {
+    margin: 10px;
+  }
+}
+</style>
